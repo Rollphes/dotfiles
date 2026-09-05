27656cbf7c45a77c6f01ccf88ba556e8e1f45f89
Rollphes <70063670+Rollphes@users.noreply.github.com>
2026-09-06 02:28:23 +0900

    feat(wezterm): restore launch profiles

--- dot_config/wezterm/launch.lua
+++ dot_config/wezterm/launch.lua
@@ -1,26 +1,83 @@
 local M = {}
 
+local function windows_environment()
+  return { MSYSTEM = 'UCRT64' }
+end
+
+local function windows_fish_args(include_environment)
+  local args = {}
+
+  if include_environment then
+    table.insert(args, 'C:\\msys64\\usr\\bin\\env.exe')
+    table.insert(args, 'MSYSTEM=UCRT64')
+  end
+
+  table.insert(args, 'C:\\msys64\\usr\\bin\\fish.exe')
+  table.insert(args, '--login')
+  table.insert(args, '-i')
+
+  return args
+end
+
+local function macos_fish_args(target)
+  if target == 'aarch64-apple-darwin' then
+    return { '/opt/homebrew/bin/fish', '--login', '-i' }
+  end
+
+  return { '/usr/local/bin/fish', '--login', '-i' }
+end
+
 function M.apply(config, wezterm)
   local target = wezterm.target_triple
 
   if target:find('windows') then
-    config.default_prog = {
-      'C:\\msys64\\usr\\bin\\fish.exe',
-      '--login',
-      '-i',
-    }
-
-    config.set_environment_variables = {
-      MSYSTEM = 'UCRT64',
-    }
-
+    config.default_prog = windows_fish_args(true)
     config.mux_enable_ssh_agent = false
-  elseif target == 'aarch64-apple-darwin' then
-    config.default_prog = { '/opt/homebrew/bin/fish', '--login', '-i' }
-  elseif target == 'x86_64-apple-darwin' then
-    config.default_prog = { '/usr/local/bin/fish', '--login', '-i' }
+    config.launch_menu = {
+      {
+        label = 'MSYS2 UCRT64 Fish',
+        args = windows_fish_args(false),
+        set_environment_variables = windows_environment(),
+      },
+      {
+        label = 'PowerShell',
+        args = { 'pwsh.exe' },
+      },
+      {
+        label = 'Windows PowerShell',
+        args = { 'powershell.exe' },
+      },
+      {
+        label = 'Command Prompt',
+        args = { 'cmd.exe' },
+      },
+    }
+  elseif target:find('apple') then
+    local fish_args = macos_fish_args(target)
+
+    config.default_prog = fish_args
+    config.launch_menu = {
+      {
+        label = 'Fish',
+        args = fish_args,
+      },
+      {
+        label = 'Zsh',
+        args = { '/bin/zsh', '-l' },
+      },
+    }
   elseif target:find('linux') then
     config.default_prog = { '/usr/bin/fish', '--login', '-i' }
+    config.launch_menu = {
+      {
+        label = 'Fish',
+        args = { '/usr/bin/fish', '--login', '-i' },
+      },
+      {
+        label = 'Default shell',
+        args = { os.getenv 'SHELL' or '/bin/sh', '-l' },
+      },
+    }
   end
 end
 
