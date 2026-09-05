917ec2683bcd437e7fbbc4dc3ed2b7558594e022
Rollphes <70063670+Rollphes@users.noreply.github.com>
2026-09-06 01:23:25 +0900

    feat(wezterm): support native shells across platforms

--- dot_config/wezterm/launch.lua
+++ dot_config/wezterm/launch.lua
@@ -1,7 +1,9 @@
 local M = {}
 
 function M.apply(config, wezterm)
-  if wezterm.target_triple:find('windows') then
+  local target = wezterm.target_triple
+
+  if target:find('windows') then
     config.default_prog = {
       'C:\\msys64\\usr\\bin\\fish.exe',
       '--login',
@@ -13,6 +15,12 @@ function M.apply(config, wezterm)
     }
 
     config.mux_enable_ssh_agent = false
+  elseif target == 'aarch64-apple-darwin' then
+    config.default_prog = { '/opt/homebrew/bin/fish', '--login', '-i' }
+  elseif target == 'x86_64-apple-darwin' then
+    config.default_prog = { '/usr/local/bin/fish', '--login', '-i' }
+  elseif target:find('linux') then
+    config.default_prog = { '/usr/bin/fish', '--login', '-i' }
   end
 end
 
