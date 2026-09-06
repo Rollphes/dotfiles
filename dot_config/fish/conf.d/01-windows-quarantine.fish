e49cdb2c3b198394a1310170df935b9b659b84e5
Rollphes <70063670+Rollphes@users.noreply.github.com>
2026-09-06 09:17:53 +0900

    fix(windows): contain native zoxide state

--- dot_config/fish/conf.d/01-windows-quarantine.fish
+++ dot_config/fish/conf.d/01-windows-quarantine.fish
@@ -9,3 +9,6 @@ set -gx LOCALAPPDATA "$HOME/.winprofile/AppData/Local"
 set -gx UV_CACHE_DIR "$XDG_CACHE_HOME/uv"
 set -gx UV_TOOL_DIR "$XDG_DATA_HOME/uv/tools"
 set -gx UV_PYTHON_INSTALL_DIR "$XDG_DATA_HOME/uv/python"
+
+# zoxide uses the Windows local application data directory by default.
+set -gx _ZO_DATA_DIR "$XDG_DATA_HOME/zoxide"
--- dot_config/fish/post.d/zoxide.fish
+++ dot_config/fish/post.d/zoxide.fish
@@ -2,8 +2,6 @@ if not command -q zoxide
     return
 end
 
-set -gx _ZO_DATA_DIR "$XDG_DATA_HOME/zoxide"
-
 set -l init_file "$XDG_CACHE_HOME/fish/init/zoxide.fish"
 
 if test -r "$init_file"
