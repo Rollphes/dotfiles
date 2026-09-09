0a9dd5422ffcae0612f1969bfafc1d85d8e64962
Rollphes <70063670+Rollphes@users.noreply.github.com>
2026-09-10 04:53:15 +0900

    feat(nvim): disable arrow keys in normal, visual, and insert modes

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,2 +1,10 @@
-local map = vim.api.nvim_set_keymap
-local opts = { noremap = true, silent = true }
+local disabled_keys = {
+    "<Up>",
+    "<Down>",
+    "<Left>",
+    "<Right>",
+}
+
+for _, key in ipairs(disabled_keys) do
+    vim.keymap.set({ "n", "v", "x", "i" }, key, "<Nop>")
+end
