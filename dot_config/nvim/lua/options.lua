e3b0ea9f0cfd4c9e9f118b8c6110b45f85c61c7e
Rollphes <70063670+Rollphes@users.noreply.github.com>
2026-09-09 19:42:58 +0900

    feat(nvim): add configuration files for options, keymaps, and highlights

---
+++ dot_config/nvim/lua/options.lua
@@ -0,0 +1,63 @@
+vim.opt.number = true
+vim.opt.relativenumber = false
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = false
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.linebreak = false
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 8
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.list = true
+vim.opt.listchars = {
+    tab = "→ ",
+    lead = "·",
+    trail = "·",
+    nbsp = "␣",
+    eol = "↲",
+}
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.autoindent = true
+vim.opt.smartindent = false
+vim.opt.breakindent = false
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+vim.opt.wrapscan = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.equalalways = true
+
+vim.opt.mouse = "a"
+vim.opt.hidden = true
+vim.opt.undofile = true
+vim.opt.undolevels = 1000
+
+vim.opt.backup = false
+vim.opt.writebackup = true
+vim.opt.swapfile = true
+vim.opt.autoread = true
+vim.opt.autowrite = false
+vim.opt.confirm = false
+vim.opt.fixendofline = true
+
+vim.opt.completeopt = { "menu", "popup" }
+vim.opt.pumheight = 0
+vim.opt.pumblend = 0
+
+vim.opt.updatetime = 4000
+vim.opt.timeout = true
+vim.opt.timeoutlen = 1000
+
+vim.opt.laststatus = 2
+vim.opt.showmode = true
+vim.opt.cmdheight = 1
+vim.opt.ruler = true
