449f67c8413af4465468f11d29ba24ef554c7839
Rollphes <70063670+Rollphes@users.noreply.github.com>
2026-09-13 01:27:11 +0900

    feat(nvim): add Oil file navigation

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -8,3 +8,7 @@ local disabled_keys = {
 for _, key in ipairs(disabled_keys) do
     vim.keymap.set({ "n", "v", "x", "i" }, key, "<Nop>")
 end
+
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", {
+    desc = "Open parent directory",
+})
--- dot_config/nvim/lua/packages.lua
+++ dot_config/nvim/lua/packages.lua
@@ -7,4 +7,5 @@ vim.pack.add({
     { src = "https://github.com/rmagatti/auto-session" },
     { src = "https://github.com/Saghen/blink.cmp" },
     { src = "https://github.com/saghen/blink.lib" }, 
+    { src = "https://github.com/stevearc/oil.nvim" },
 })
