02092a0a15259e000c38a4fb1a2bac0577bc6974
Rollphes <70063670+Rollphes@users.noreply.github.com>
2026-09-14 22:53:23 +0900

    chore(nvim): restore arrow key navigation

--- dot_config/nvim/lua/keymaps.lua
+++ dot_config/nvim/lua/keymaps.lua
@@ -1,14 +1,3 @@
-local disabled_keys = {
-    "<Up>",
-    "<Down>",
-    "<Left>",
-    "<Right>",
-}
-
-for _, key in ipairs(disabled_keys) do
-    vim.keymap.set({ "n", "v", "x", "i" }, key, "<Nop>")
-end
-
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", {
     desc = "Open parent directory",
 })
