9f6bd2fd599b5145cbbcf429057a7f5e98be444c
Rollphes <70063670+Rollphes@users.noreply.github.com>
2026-09-15 00:12:22 +0900

    fix(nvim): prevent message pager focus steal

--- dot_config/nvim/lua/plugins/tiny_cmdline.lua
+++ dot_config/nvim/lua/plugins/tiny_cmdline.lua
@@ -1,4 +1,12 @@
-require("vim._core.ui2").enable({})
+require("vim._core.ui2").enable({
+    msg = {
+        -- Keep ordinary command output out of the cmdline window. With
+        -- cmdheight=0, expanded cmdline messages treat the next Enter as a
+        -- request to focus the pager. The ephemeral msg window is
+        -- non-focusable; message history remains available via :messages/g<.
+        targets = "msg",
+    },
+})
 
 require("tiny-cmdline").setup({
     width = {
