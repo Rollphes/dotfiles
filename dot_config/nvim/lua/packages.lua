107ed9e1c457b6cdf6e91f60e371cadd63f0bf9c
Rollphes <70063670+Rollphes@users.noreply.github.com>
2026-09-14 22:54:04 +0900

    feat(nvim): add automatic tag editing

--- dot_config/nvim/lua/packages.lua
+++ dot_config/nvim/lua/packages.lua
@@ -17,4 +17,5 @@ vim.pack.add({
     { src = "https://github.com/petertriho/nvim-scrollbar" },
     { src = "https://github.com/rachartier/tiny-inline-diagnostic.nvim" },
     { src = "https://github.com/rachartier/tiny-cmdline.nvim" },
+    { src = "https://github.com/windwp/nvim-ts-autotag" },
 })
