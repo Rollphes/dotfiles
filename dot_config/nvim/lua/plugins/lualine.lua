448a8710609d255f76d60e6bf8e7a2e5c84c8c71
Rollphes <70063670+Rollphes@users.noreply.github.com>
2026-09-17 09:44:58 +0900

    `feat(nvim): show pending key sequence in lualine`

--- dot_config/nvim/lua/plugins/lualine.lua
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -1,3 +1,6 @@
+vim.opt.showcmd = true
+vim.opt.showcmdloc = "statusline"
+
 require("lualine").setup({
     options = {
         theme = "auto",
@@ -9,7 +12,13 @@ require("lualine").setup({
         lualine_b = { "branch", "diff", "diagnostics" },
         lualine_c = {},
 
-        lualine_x = { "filetype" },
+        lualine_x = {
+            {
+                "%S",
+                padding = { left = 1, right = 1 },
+            },
+            "filetype",
+        },
         lualine_y = { "progress" },
         lualine_z = { "location" },
     },
