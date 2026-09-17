8dc62b6c66f8343251533675a9b2fce5c4adfaa7
Rollphes <70063670+Rollphes@users.noreply.github.com>
2026-09-17 09:28:35 +0900

    feat(nvim): show Oil path in dropbar

--- dot_config/nvim/lua/plugins/dropbar.lua
+++ dot_config/nvim/lua/plugins/dropbar.lua
@@ -1,1 +1,60 @@
-require("dropbar").setup()
+local bar = require("dropbar.bar")
+local sources = require("dropbar.sources")
+local utils = require("dropbar.utils")
+
+local oil_source = {
+    get_symbols = function(buf, _, _)
+        local ok, oil = pcall(require, "oil")
+        if not ok then
+            return {}
+        end
+
+        local dir = oil.get_current_dir(buf)
+        if not dir then
+            return {}
+        end
+
+        dir = vim.fn.fnamemodify(dir, ":~")
+
+        return {
+            bar.dropbar_symbol_t:new({
+                icon = "󰉋 ",
+                name = dir,
+                name_hl = "Directory",
+            }),
+        }
+    end,
+}
+
+require("dropbar").setup({
+    bar = {
+        sources = function(buf, _)
+            if vim.bo[buf].filetype == "oil" then
+                return {
+                    oil_source,
+                }
+            end
+
+            if vim.bo[buf].filetype == "markdown" then
+                return {
+                    sources.path,
+                    sources.markdown,
+                }
+            end
+
+            if vim.bo[buf].buftype == "terminal" then
+                return {
+                    sources.terminal,
+                }
+            end
+
+            return {
+                sources.path,
+                utils.source.fallback({
+                    sources.lsp,
+                    sources.treesitter,
+                }),
+            }
+        end,
+    },
+})
